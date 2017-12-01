e9df1ed17ea0ccdca2143eec0ece9b21d82c2173
Florin Vasile <fvasi88@gmail.com>
2017-12-01 23:07:40 +0100

    Attempt at a KO-Rest driven app

--- src/main/java/eu/fvsware/jpaexplorer/JPAExplorerApplication.java
+++ src/main/java/eu/fvsware/jpaexplorer/JPAExplorerApplication.java
@@ -1,6 +1,7 @@
 package eu.fvsware.jpaexplorer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 @Controller
 @ComponentScan("eu.fvsware")
+@EnableAutoConfiguration
 public class JPAExplorerApplication {
 	
 	@RequestMapping(path = "/test")
@@ -17,6 +19,11 @@ public class JPAExplorerApplication {
 	public String home() {
 		return "Hello Duke";
 	}
+
+//	@RequestMapping(path = "/index")
+//	public String index() {
+//		return "index";
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JPAExplorerApplication.class, args);
